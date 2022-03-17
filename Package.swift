a4ffeeab1227ec906abe8b24176de955017d55ca
Tom K <tomknighton@icloud.com>
2022-03-17 13:49:20 +0000

    On main: Auto stash before rebase of "refs/heads/main"

--- Package.swift
+++ Package.swift
@@ -20,9 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GoLondonSDK",
-            dependencies: []),
-        .testTarget(
-            name: "GoLondonSDKTests",
-            dependencies: ["GoLondonSDK"]),
+            dependencies: [],
+        path: "Sources"),
+        .testTarget(name: "GoLondonSDKTests", dependencies: ["GoLondonSDK"], path: "Tests")
     ]
 )
